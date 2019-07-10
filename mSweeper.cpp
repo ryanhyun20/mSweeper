2877af6f00a2780e6647f32535fb4bc748fd4fcd
ryanhyun20 <ryan0304hyun@gmail.com>
2019-07-09 20:58:13 -0700

    cin implementation of gameplay rather than ncurses real-time keypress

--- mSweeper.cpp
+++ mSweeper.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <cstdlib>
 #include <cstdio>
-#include <curses.h>
 using namespace std;
 
 struct myTile {
@@ -20,7 +19,6 @@ const int maxDim = 30;
 bool gameOver = false;
 
 myTile theGrid[maxDim][maxDim];
-string visGrid[maxDim][maxDim];
 myCursor theCursor;
 
 void initGame(void)
@@ -28,68 +26,67 @@ void initGame(void)
   theCursor.x = 0;
   theCursor.y = 0;
   for(int i = 0; i < maxDim; i++) {
-      for(int j = 0; j < maxDim; j++) {
-          visGrid[i][j] = " ";
-          theGrid[i][j].tile_value = "0";
-
-      }
+    for(int j = 0; j < maxDim; j++) {
+      theGrid[i][j].tile_value = "0";
+    }
   }
 }
 
 void setUpGrid(void)
 {
-  vector<string> placedMines;
-  int numMines = maxDim/3;
+  string placedMines[maxDim];
+  int numMines = maxDim;
+  int mineNo = 0;
   while(true) {
     //randomly generate seeds
     int x = rand() % 30;
     int y = rand() % 30;
     string potentialMine = std::to_string(x) + ", " + std::to_string(y);
     bool place = true;
-    for(string item: placedMines) {
-      if(item.compare(potentialMine) == 0) {
+    for(int i = 0; i < maxDim/3; i++) {
+      if(placedMines[i].compare(potentialMine) == 0) {
         place = false;
       }
     }
     if(place) {
-      placedMines.push_back(potentialMine);
+      placedMines[mineNo] = potentialMine;
+      mineNo++;
       theGrid[x][y].tile_value = "X";
       for(int i = x - 1; i < x + 2; i++) {
         for(int j = y - 1; j < y + 2; j++) {
           if(i == x && j == y) {
             theGrid[i][j].tile_value = "X";
-          } else if(i > -1 && i < maxDim && j > -1 && j < maxDim) {
+          } else if(i > -1 && i < maxDim && j > -1 && j < maxDim &&
+                    theGrid[i][j].tile_value.compare("X") != 0) {
             theGrid[i][j].tile_value = std::to_string(stoi(theGrid[i][j].tile_value) + 1);
           }
         }
       }
-      numMines--;
-      if(numMines == 0) {
+      if(numMines == mineNo) {
         break;
       }
     }
   }
 }
 
-void moveCursor(int x, int y, int dir)
+void moveCursor(string key)
 {
-  switch(dir) {
-    case 72:
-      if(theCursor.y > 0) {
-        theCursor.y--;
-      }
-    case 80:
-      if(theCursor.y < maxDim - 1) {
-        theCursor.y++;
-      }
-    case 75:
-      if(theCursor.x > 0) {
-        theCursor.x--;
-      }
-    case 77:
-      if(theCursor.x < maxDim - 1) {
-          theCursor.x++;
-      }
+  if(key.compare("k") == 0) {
+    if(theCursor.y > 0) {
+      theCursor.y--;
+    }
+  } else if(key.compare("i") == 0) {
+    if(theCursor.y < maxDim - 1) {
+      theCursor.y++;
+    }
+  } else if(key.compare("j") == 0) {
+    if(theCursor.x > 0) {
+      theCursor.x--;
+    }
+  } else if(key.compare("l") == 0) {
+    if(theCursor.x < maxDim - 1) {
+        theCursor.x++;
+    }
   }
 }
 
@@ -105,7 +102,7 @@ bool isComplete(int x, int y)
       }
     }
   }
-  if(std:to_string(flags).compare(theGrid[x][y].tile_value) == 0) {
+  if(std::to_string(flags).compare(theGrid[x][y].tile_value) == 0) {
       return true;
   }
   return false;
@@ -118,7 +115,7 @@ void dig(int x, int y, int depth)
       for(int i = x - 1; i < x + 2; i++) {
         for(int j = y - 1; j < y + 2; j++) {
           if(i > -1 && i < maxDim && j > -1 && j < maxDim) {
-            dig(x, y, depth);
+            dig(i, j, depth);
           }
         }
       }
@@ -150,24 +147,37 @@ void printGrid(void)
   for(int i = 0; i < maxDim * 2 - 2; i++) {
     cout << "-";
   }
-  cout << "+";
+  cout << "-+\n";
   for(int i = 0; i < maxDim; i++) {
     cout << "|";
     for(int j = 0; j < maxDim; j++) {
-      cout << visGrid[i][j] << "|";
+      if(theGrid[i][j].flag) {
+        cout << "F" << "|";
+      } else if(theGrid[i][j].tile_value.compare("0") != 0) {
+        cout << theGrid[i][j].tile_value; << "|";
+      } else {
+        cout << " " << "|";
+      }
     }
     cout << "\n";
-    cout << "|";
-    for(int j = 0; j < maxDim * 2 - 2; j++) {
-      cout << "-";
+    if(i != maxDim - 1) {
+      cout << "|";
+      for(int j = 0; j < maxDim * 2 - 2; j++) {
+        cout << "-";
+      }
+      cout << "-|" << "\n";
     }
-    cout << "|" << "\n";
   }
+  cout << "+";
+  for(int i = 0; i < maxDim * 2 - 2; i++) {
+      cout << "-";
+  }
+  cout << "-+" << "\n";
 }
 
 void isGameOver(void)
 {
-  int signals;
+  int signals = 0;
   for(int i = 0; i < maxDim; i++) {
     for(int j = 0; j < maxDim; j++) {
       if(!theGrid[i][j].hit && theGrid[i][j].tile_value.compare("X") != 0) {
@@ -185,39 +195,52 @@ void printEndGrid(void) {
   for(int i = 0; i < maxDim * 2 - 2; i++) {
     cout << "-";
   }
-  cout << "+";
+  cout << "-+\n";
   for(int i = 0; i < maxDim; i++) {
     cout << "|";
     for(int j = 0; j < maxDim; j++) {
       if(theGrid[i][j].tile_value.compare("X") == 0) {
         cout << theGrid[i][j].tile_value << "|";
       } else {
-        cout << visGrid[i][j] << "|";
+        cout << " " << "|";
       }
     }
     cout << "\n";
-    cout << "|";
-    for(int j = 0; j < maxDim * 2 - 2; j++) {
-      cout << "-";
+    if(i != maxDim - 1) {
+      cout << "|";
+      for(int j = 0; j < maxDim * 2 - 2; j++) {
+        cout << "-";
+      }
+      cout << "-|" << "\n";
     }
-    cout << "|" << "\n";
   }
+  cout << "+";
+  for(int i = 0; i < maxDim * 2 - 2; i++) {
+      cout << "-";
+  }
+  cout << "-+" << "\n";
 }
 
 int main()
 {
   initGame();
   setUpGrid();
+  printGrid();
   //game loop
   while(true) {
-    printGrid();
-    char key = getch();
-    if(key > 71) {
-      moveCursor(theCursor.x, theCursor.y, key);
-    } else if(key == 64){
+    string key;
+    cout << "Enter an action(f to flag, d to dig, ijkl to move): ";
+    cin >> key;
+    if(key.compare("i") == 0 || key.compare("j") == 0 || key.compare("k") == 0
+        ||key.compare("l") == 0) {
+      moveCursor(key);
+      printEndGrid();
+    } else if(key.compare("d") == 0){
       dig(theCursor.x, theCursor.y, 0);
-    } else if(key == 66) {
+      printGrid();
+    } else if(key.compare("f") == 0) {
       flag(theCursor.x, theCursor.y);
+      printGrid();
     }
     if(!gameOver) {
       isGameOver();
